789040b0bb69b8b7ab142ab1fcffc627f121f547
Marta Khoma <tyxa.khoma16@gmail.com>
2021-10-25 16:41:20 +0300

    Added 3rd tip to task 3

--- Homework3/src/hometask3.java
+++ Homework3/src/hometask3.java
@@ -25,6 +25,27 @@ public class hometask3 {
         String  adress = in.next();
         System.out.println("Your name is "+ name +" and your native adress is " + adress);
 
+        //Task 3
+        double c1=7.9;
+        double c2=6;
+        double c3=13.5;
+
+        int t1 = 23;
+        int t2 = 93;
+        int t3 = 100;
+
+        System.out.println("c1*t1 = " + c1*t1);
+        System.out.println("c2*t2 = " + c2*t2);
+        System.out.println("c3*t3 = " + c3*t3);
+
+        System.out.println("Sum = " + c1*t1+c2*t2+c3*t3);
+
+
+
+
+
+
+
 
     }
 }
